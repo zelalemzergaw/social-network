d0d6233aef4e13727ee5c66c1963aa3e3ba4985a
Solmn <solomonmolla10@gmail.com>
2020-04-13 23:17:06 -0500

    Added Jwt and error interceptors.

---
+++ Client/src/app/util/jwt.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AuthenticationService } from '../services';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    constructor(private authenticationService: AuthenticationService) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // add authorization header with jwt token if available
+        let currentUser = this.authenticationService.getCurrentUser();
+        if (currentUser && currentUser.access_token) {
+            request = request.clone({
+                setHeaders: {
+                    "access-token": currentUser.access_token
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
