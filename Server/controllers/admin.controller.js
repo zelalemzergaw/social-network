690e5ff4e1fcfd49009ddeb1ce56c170028986ed
Gizaw <gizawroba@gmail.com>
2020-04-13 20:04:46 -0500

    Add create advertisement service
    Add

--- Server/controllers/admin.controller.js
+++ Server/controllers/admin.controller.js
@@ -28,3 +28,20 @@ exports.removeBadWord = (req, res, next) => {
 exports.updateBadWord = (req, res, next) => {
     adminService.updateBadWordList(req.body);
 }
+async function addAdvertisement(id,advData) {
+
+    const advertisement = new Ad({
+       text : advData.text,
+       link:advData.link,
+       postby: advData.postby,
+       datepublished:advData.datepublished,
+       targetedUser: advData.age,
+       targetedUser: advData.location
+    });
+        
+     let ad_ =   await advertisement.save();
+     
+     await userService.updateUser(id, {adId:ad_._id});
+    
+    
+    }
