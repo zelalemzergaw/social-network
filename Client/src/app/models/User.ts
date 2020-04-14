b707443adb79d8aa6dd4e6a434aba8f745a1adb5
Solmn <solomonmolla10@gmail.com>
2020-04-13 19:08:00 -0500

    Added user model

---
+++ Client/src/app/models/User.ts
@@ -0,0 +1,10 @@
+export class User {
+  _id: number,
+  username: string,
+  firstname: string,
+  lastname: string,
+  birthdate: string,
+  location: string,
+  role: string,
+  access_token?: string
+}
