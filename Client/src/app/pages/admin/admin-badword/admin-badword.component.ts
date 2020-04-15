ad85bb045a2d9123490bfbbfd8614c97b8bf1d7a
Nathanael Birri <nathichuta@gmail.com>
2020-04-14 19:17:18 -0500

    Admin bad word component view

---
+++ Client/src/app/pages/admin/admin-badword/admin-badword.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-badword',
+  templateUrl: './admin-badword.component.html',
+  styleUrls: ['./admin-badword.component.scss']
+})
+export class AdminBadwordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
